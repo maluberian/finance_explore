ad98440a24a143290bcf901d77f0353963e33d2c
Dustin Clifford <dclifford@sitesoftllc.net>
2024-09-28 16:24:32 -0400

    adding environemental setup and some documentation

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # Yahoo! Finance - Initial API Exploration
 
-# Description
-Here we will play with some **Finance** tooling. 
+## Description
+Here we will play with some **Finance** tooling: 
+
+* [Tiingo](https://www.tiingo.com/)
+
+## Project setup
+Be sure to run the script found in `./scripts/setup.sh`
+
+### Initial Setup
+```console
+$ ./scripts/setup.sh init
+```
+
+### Setup the environment
+```console
+$ ./scripts/setup.sh env
+```
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,18 +1,43 @@
 #!/usr/bin/env bash
 
-init() {
-  if [[ -x $(which python) ]]
+function setup_python() {
+  if [[ ! -d ./.venv ]]
   then
-    echo "Python exists!"
+    echo "Setting up the .venv virtual environment"
+    python -m venv .venv
+  fi
+  source .venv/bin/activate
+  pip install --upgrade pip
+  pip install --upgrade pandas
+  pip install --upgrade plotly
+}
+
+function setup() {
+  # Check that we have python installed and that it is Python 3
+  if [[ -x $(which python) && $(python -V) =~ ^Python\ 3 ]]
+  then
+    echo "Preparing python environment"
+    setup_python
   else
-    echo "No Python :("
+    >&2 echo "No Python 3 :( - Please, install python to continue"
+    exit
   fi
 }
 
+function env() {
+  if [[ ! -f .env ]]
+  then
+    >&2 echo  "No environment found. Please, create one by copying env.tmpl to .env"
+  fi
+  source ./.env
+}
+
 case $1 in
-  "init")
-    # shellcheck disable=SC1073
-    init()
+  init)
+    setup
+    ;;
+  env)
+    env
     ;;
   *)
     echo "Nothing to do"
